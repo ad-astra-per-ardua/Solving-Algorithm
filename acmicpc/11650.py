df03cb11d4e32f4e81d97f6c46a8f07c58b80404
d982h8st7 <sinnarver15@gmail.com>
2023-05-03 13:09:25 +0900

    In problem, we should sort x's y's position by asc. In that case used sort() and using for() loop for n range.

---
+++ acmicpc/11650.py
@@ -0,0 +1,13 @@
+
+n = int(input().strip())
+points = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+points.sort()
+
+for point in points:
+    print(point[0], point[1])
+
