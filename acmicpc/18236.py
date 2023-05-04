25ec7e1390d0301853ccb724621c2b0aca5eafee
d982h8st7 <sinnarver15@gmail.com>
2023-05-04 11:15:38 +0900

    In the previous code, there were some unused variables and unnecessary code. In the updated code, these issues were fixed to simplify and optimize the solution. Additionally, some variables were renamed for better clarity due to memory overflow. Overall, the functionality of the code remained the same.

--- acmicpc/18236.py
+++ acmicpc/18236.py
@@ -1,18 +1,19 @@
-def matrix_chain_order(matrices):
-    n = len(matrices)
-    m = [[0 if i == j else float('inf') for j in range(n)] for i in range(n)]
+import sys
 
-    for l in range(2, n + 1):
-        for i in range(n - l + 1):
-            j = i + l - 1
-            for k in range(i, j):
-                q = m[i][k] + m[k + 1][j] + matrices[i][0] * matrices[k][1] * matrices[j][1]
-                if q < m[i][j]:
-                    m[i][j] = q
+n = int(sys.stdin.readline().rstrip())
+matrix_sizes = []
+for _ in range(n):
+    r, c = map(int, sys.stdin.readline().rstrip().split())
+    matrix_sizes.append((r, c))
 
-    return m[0][n - 1]
+dp = [[float('inf')] * n for _ in range(n)]
+for i in range(n):
+    dp[i][i] = 0
 
-N = int(input())
-matrices = [list(map(int, input().split())) for _ in range(N)]
+for k in range(1, n):
+    for i in range(n - k):
+        j = i + k
+        for m in range(i, j):
+            dp[i][j] = min(dp[i][j], dp[i][m] + dp[m+1][j] + matrix_sizes[i][0] * matrix_sizes[m][1] * matrix_sizes[j][1])
 
-print(matrix_chain_order(matrices))
+print(dp[0][n-1])
