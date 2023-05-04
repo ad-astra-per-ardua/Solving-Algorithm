c13328017a783532f7036a2120e41629cf18d2bc
d982h8st7 <sinnarver15@gmail.com>
2023-05-04 12:21:22 +0900

    Used 1st dimension array to minimize allocate memory m's matrics.

--- acmicpc/18236.py
+++ acmicpc/18236.py
@@ -1,22 +1,19 @@
 def matrix_chain_order(matrices):
     n = len(matrices)
-    m = [[0] * n for _ in range(n)]
+    m = [0] * n
 
-    def cost(i, j, k):
+    def cost(i, k, j):
         return matrices[i][0] * matrices[k][1] * matrices[j][1]
 
     def hu_shing_algorithm(i, j):
-        if m[i][j] > 0:
-            return m[i][j]
         if i == j:
-            m[i][j] = 0
-        else:
-            m[i][j] = float('inf')
-            for k in range(i, j):
-                q = hu_shing_algorithm(i, k) + hu_shing_algorithm(k + 1, j) + cost(i, k, j)
-                if q < m[i][j]:
-                    m[i][j] = q
-        return m[i][j]
+            return 0
+        min_cost = float('inf')
+        for k in range(i, j):
+            q = hu_shing_algorithm(i, k) + hu_shing_algorithm(k + 1, j) + cost(i, k, j)
+            if q < min_cost:
+                min_cost = q
+        return min_cost
 
     return hu_shing_algorithm(0, n - 1)
 
