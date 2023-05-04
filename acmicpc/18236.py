71e5b1301db221a3ae4b92d3a87662bad417af3f
d982h8st7 <sinnarver15@gmail.com>
2023-05-04 14:01:56 +0900

    ㅁㄷㅈㄹㅁㅈㄷ려묳ㄹㅁㄷ죠맺댜ㅕㅛ랴ㅕㅐㅁㅈ댜ㅐ굠ㄹㅎ

--- acmicpc/18236.py
+++ acmicpc/18236.py
@@ -1,16 +1,23 @@
+import sys
+sys.setrecursionlimit(10**9)
 def matrix_chain_order(matrices):
     n = len(matrices)
-    m = [[0 if i == j else float('inf') for j in range(n)] for i in range(n)]
+    m = [0] * n
 
-    for l in range(2, n + 1):
-        for i in range(n - l + 1):
-            j = i + l - 1
-            for k in range(i, j):
-                q = m[i][k] + m[k + 1][j] + matrices[i][0] * matrices[k][1] * matrices[j][1]
-                if q < m[i][j]:
-                    m[i][j] = q
+    def cost(i, k, j):
+        return matrices[i][0] * matrices[k][1] * matrices[j][1]
 
-    return m[0][n - 1]
+    def hu_shing_algorithm(i, j):
+        if i == j:
+            return 0
+        min_cost = float('inf')
+        for k in range(i, j):
+            q = hu_shing_algorithm(i, k) + hu_shing_algorithm(k + 1, j) + cost(i, k, j)
+            if q < min_cost:
+                min_cost = q
+        return min_cost
+
+    return hu_shing_algorithm(0, n - 1)
 
 N = int(input())
 matrices = [list(map(int, input().split())) for _ in range(N)]
