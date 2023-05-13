3e2910db10dea9cbb91bb76ffb5d97daef6b4b65
d982h8st7 <sinnarver15@gmail.com>
2023-05-13 22:45:39 +0900

    연ㄴ습ㅂ장ㅇ

--- Lecture.py
+++ Lecture.py
@@ -1,17 +1,33 @@
-loop = int(input())
-lists = []
-list2 = []
+import sys
 
-for _ in range(loop):
-    lists.append(input())
+n = int(sys.stdin.readline().strip())
+list1 = list(map(int,sys.stdin.readline().split()))
+list1.sort()
+n2 = int(sys.stdin.readline().strip())
+list2 = list(map(int,sys.stdin.readline().split()))
+#
+#
+# for i in list2:
+#     if i in list1:
+#         print('1')
+#     else:
+#         print('0')
 
-length = 1
-lists = sorted(set(lists))
-list3 = lists.copy()
 
-while list3:
-    for i in list3[:]:
-        if len(i) == length:
-            list2.append(i)
-            list3.remove(i)
-    length =+ 1
+def b_search(arr ,value):
+    first , last = 0, len(arr)-1
+    while first <= last:
+        mid = (first + last) // 2
+        if arr[mid] == value:
+            return mid
+        if arr[mid] > value:
+            last = mid - 1
+        else:
+            first = mid + 1
+
+for i in list2:
+    result = b_search(list1, i)
+    if result is None:
+        print('0')
+    else:
+        print('1')
