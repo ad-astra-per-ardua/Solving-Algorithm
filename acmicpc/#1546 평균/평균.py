fb3fb73f567b6f3c1612c25e16fc5a3112aea619
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-05-22 20:38:38 +0900

    [Bronze I 1546] Title: 평균, Time: 76 ms, Memory: 36220 KB -NKLCBHub

--- acmicpc/#1546 평균/평균.py
+++ acmicpc/#1546 평균/평균.py
@@ -1,8 +1,14 @@
-import sys
+import sys,math
+from collections import deque
 input = sys.stdin.readline
 
+fin_score = deque()
 prob = int(input().rstrip())
-scores = list(map(int, input().split()))
-max_score = max(scores)
-avg = sum((s / max_score * 100 for s in scores)) / prob
+score = deque(map(int,input().split()))
+copy = tuple(score)
+for i in copy:
+    temp = score.popleft()
+    temp2 = temp / max(copy) * 100
+    fin_score.append(temp2)
+avg = sum(fin_score) / len(fin_score)
 print(avg)
