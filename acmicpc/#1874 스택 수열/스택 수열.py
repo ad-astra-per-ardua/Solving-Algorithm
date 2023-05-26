90f5d31cc84afb4bb9c8e6908f8ada05e2d666a3
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-05-27 04:59:40 +0900

    [Silver II 1874] Title: 스택 수열, Time: 116 ms, Memory: 33624 KB -NKLCBHub

---
+++ acmicpc/#1874 스택 수열/스택 수열.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stack, result, cnt = [], [], 1
+for i in range(n):
+    num = int(input())
+    while cnt <= num:
+        stack.append(cnt)
+        result.append("+")
+        cnt += 1
+    if stack[-1] == num:
+        stack.pop()
+        result.append("-")
+    else:
+        print("NO")
+        exit(0)
+print('\n'.join(result))
