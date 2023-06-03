4632be8508723f0908059ef5010c2b4379073510
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-06-04 06:39:54 +0900

    [Silver I 2178] Title: 미로 탐색, Time: 76 ms, Memory: 34308 KB -NKLCBHub

---
+++ acmicpc/#2178 미로 탐색/미로 탐색.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+
+def bfs(y,x):
+    queue = deque()
+    queue.append((y, x))
+    while queue:
+        cy, cx = queue.popleft()
+        for k in range(4):
+            ny = cy + direction_y[k]
+            nx = cx + direction_x[k]
+            if 0 <= ny < n and 0 <= nx < m:
+                if array[ny][nx] == 1 and visited[ny][nx] == False:
+                    queue.append((ny,nx))
+                    visited[ny][nx] = visited[cy][cx] + 1
+    return visited[n-1][m-1]
+
+
+n,m = map(int,input().split())  # m = 가로 n = 세로
+direction_x = [1,0,-1,0]
+direction_y = [0,1,0,-1]
+array = []
+visited = [[0] * m for _ in range(n)]
+
+for _ in range(n):
+    array.append(list(map(int, list(input().strip()))))
+
+visited[0][0] = 1
+print(bfs(0,0))
