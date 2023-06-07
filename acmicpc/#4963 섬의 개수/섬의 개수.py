0341ff7918580f158e653bd0e5f0138ca84d658d
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-06-07 16:03:19 +0900

    [Silver II 4963] Title: 섬의 개수, Time: 60 ms, Memory: 31256 KB -NKLCBHub

--- acmicpc/#4963 섬의 개수/섬의 개수.py
+++ acmicpc/#4963 섬의 개수/섬의 개수.py
@@ -1,37 +1,40 @@
 import sys
-input = sys.stdin.readline
 
+input = sys.stdin.readline
+#  add direction with 4 direct + diagonally
 direction_x = [1, 0, -1, 0, 1, 1, -1, -1]
 direction_y = [0, 1, 0, -1, 1, -1, 1, -1]
-# setting data structures
+
 
 def bfs(x, y):
     queue = [(x, y)]
     while queue:
         now_x, now_y = queue.pop()
-        for k in range(8):
+        for k in range(8):  # search 8 direction
             nx = now_x + direction_x[k]
             ny = now_y + direction_y[k]
-            if 0 <= nx < w and 0 <= ny < h:
+            if 0 <= nx < w and 0 <= ny < h:  # search direction if it's out of range
                 if array[ny][nx] == 1 and visited[ny][nx] == False:
-                    visited[ny][nx] = True
-                    queue.append((nx, ny))
+                    visited[ny][nx] = True  # marked that point as visited
+                    queue.append((nx, ny))  # append next position in queue
     return
 
 
 while True:
     w, h = map(int, input().split())
-    array = [list(map(int, input().split())) for _ in range(h)]
+    array = [list(map(int, input().split())) for _ in range(h)]  # make array size as w X h
     visited = [[False] * w for _ in range(h)]
+    # setting data structures
+
     count = 0
     if w == 0 and h == 0:
         break
+    # setting breakpoint
+
     for i in range(h):
         for j in range(w):
-            if array[i][j] == 1 and visited[i][j] == False:
+            if array[i][j] == 1 and visited[i][j] == False:  # if starting point is 1 , and not visited = execute bfs
                 visited[i][j] = True
-                count += 1
+                count += 1  # if find island, add 1 on count
                 bfs(j, i)
     print(count)
-
-
