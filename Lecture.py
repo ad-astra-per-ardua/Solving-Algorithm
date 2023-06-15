a98089a78daa059027661b75506e2b20dca95a22
d982h8st7 <sinnarver15@gmail.com>
2023-06-15 10:45:30 +0900

    dd

--- Lecture.py
+++ Lecture.py
@@ -1,25 +1,28 @@
-import sys
+def make_spiral_matrix(N):
+    matrix = [[0]*N for _ in range(N)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
 
-input = sys.stdin.readline
+    num = 1
+    x, y = 0, 0
+    direction = 0
 
-loop = int(input())
-paper = []
-temp = []
-overlap = []
+    while num <= N*N:
+        matrix[x][y] = num
+        num += 1
 
+        nx, ny = x + dx[direction], y + dy[direction]
 
-for _ in range(loop):
-    a, b = map(int, input().split())
-    paper.append((a, b))
-    for _ in range(1):
-        paper.append((a + 10, b + 10))
+        if 0 <= nx < N and 0 <= ny < N and matrix[nx][ny] == 0:
+            x, y = nx, ny
+        else:
+            direction = (direction + 1) % 4
+            x, y = x + dx[direction], y + dy[direction]
 
+    return matrix
 
-for i in range(len(paper)):
-    a, b = paper[i // 2 - 1]
-    if
+N = int(input())
+matrix = make_spiral_matrix(N)
+row, col = map(int,input().split())
 
-
-print(paper)
-print(len(paper))
-print(a,b)
+print(matrix[row-1][col-1])
