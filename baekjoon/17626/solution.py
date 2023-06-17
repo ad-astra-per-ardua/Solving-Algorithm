d8b825078ce910b018c4d9a2df57bb2b95c32269
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-06-17 10:06:21 +0900

    uploaded at 2023. 6. 17. 오전 10:06:17

---
+++ baekjoon/17626/solution.py
@@ -0,0 +1,21 @@
+// [문제 링크]: https://www.acmicpc.net/problem/17626
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+array = [0 if i ** 0.5 % 1 else 1 for i in range(n + 1)]
+​
+minimum = 4
+for i in range(int(n ** 0.5), 0, -1):
+    if array[n]:
+        minimum = 1
+        break
+    elif array[n - i ** 2]:
+        minimum = 2
+        break
+    else:
+        for j in range(int((n - i ** 2) ** 0.5), 0, -1):
+            if array[(n - i ** 2) - j ** 2]:
+                minimum = 3
+print(minimum)
+​
