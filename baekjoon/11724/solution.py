27fd4319e6beee2c14f25cd0e8b310dd905559b5
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-06-17 10:27:21 +0900

    uploaded at 2023. 6. 17. 오전 10:27:17

---
+++ baekjoon/11724/solution.py
@@ -0,0 +1,27 @@
+// [문제 링크]: https://www.acmicpc.net/problem/11724
+
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+​
+def dfs(v):
+    visited[v] = True
+    for e in graph[v]:
+        if visited[e] == False:
+            dfs(e)
+​
+​
+n, m = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+count =0
+​
+for _ in range(m):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for i in range(1,n+1):
+    if visited[i] == False:
+        dfs(i)
+        count += 1
+print(count)
