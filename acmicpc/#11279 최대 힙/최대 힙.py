4ffbbf34244b5096f7756ffc850a0939eb1c75b2
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-06-25 06:55:13 +0900

    [Silver II 11279] Title: 최대 힙, Time: 184 ms, Memory: 46396 KB -NKLCBHub

---
+++ acmicpc/#11279 최대 힙/최대 힙.py
@@ -0,0 +1,17 @@
+import heapq as hp
+import sys
+input = sys.stdin.readline
+
+heap = []
+n = int(input().rstrip())
+
+for _ in range(n):
+    try:
+        integer = int(input().rstrip())  # if n is int = heappush, n is 0 = heappop and if is in nothing, print 0
+        if integer == 0:
+            print(hp.heappop(heap)[1])
+        else:
+            hp.heappush(heap,(-integer,integer))
+    except IndexError:
+        print(0)
+        continue
