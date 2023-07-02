e3dbba7c3691b4368468aad70afecfebece257c0
d982h8st7 <sinnarver15@gmail.com>
2023-07-03 01:18:26 +0900

    7/3

--- acmicpc/17298.py
+++ acmicpc/17298.py
@@ -1,14 +1,25 @@
-import sys
+def solve():
+    K = int(input().strip())
+    S = input().strip()
 
-n = int(sys.stdin.readline())
-a = list(map(int, input().split()))
+    answer, count_N, count_S = 0, [0]*(K+1), [0]*(K+1)
+    for i in range(1, K+1):
+        if S[i-1] == 'N':
+            count_N[i] = count_N[i-1] + 1
+            count_S[i] = count_S[i-1]
+        else:
+            count_N[i] = count_N[i-1]
+            count_S[i] = count_S[i-1] + 1
 
-result = [-1] * n
-stack = []
+    start, end = 0, 0
+    while end <= K:
+        if count_N[end] == count_S[end]:
+            half = (end - start) // 2
+            if S[start] != S[start+half]:
+                answer = max(answer, end - start)
+            start = end
+        end += 1
 
-for i in range(n):
-    while stack and a[stack[-1]] < a[i]:
-        result[stack.pop()] = a[i]
-    stack.append(i)
+    print(answer)
 
-print(*result)
+solve()
