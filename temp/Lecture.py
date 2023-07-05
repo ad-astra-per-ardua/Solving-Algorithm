9f1c69318d3d91f9bd0a7d6942dcd1ef6b196593
d982h8st7 <sinnarver15@gmail.com>
2023-07-06 06:07:35 +0900

    Complete clone coding '당근마켓' and GET, POST Using python, Javascript

--- temp/Lecture.py
+++ temp/Lecture.py
@@ -1,9 +1,26 @@
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    S = input()
-    idx = S.rfind('a')
-    if idx != -1 and S[idx:] > S[:idx]:
-        print("Yes")
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+hgt = []
+wid = []
+tot = []
+for i in range(6):
+    direction, length = map(int, sys.stdin.readline().split())
+    if direction == 1 or direction ==2:
+        wid.append(length)
+        tot.append(length)
     else:
-        print("No")
+        hgt.append(length)
+        tot.append(length)
+
+b = max(hgt) * max(wid)
+
+mh = tot.index(max(hgt))
+mw = tot.index(max(wid))
+
+s1 = abs(tot[mh-1] - tot[(mh-5 if mh == 5 else mh +1)])
+
+s2 = abs(tot[mw-1] - tot[(mw-5 if mw == 5 else mw +1)])
+ans = (b - (s1 * s2)) * t
+print(ans)
