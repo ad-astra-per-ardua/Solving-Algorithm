5787b2646d1ca9fb6fbd810eb3cf4b06006a77be
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-07-25 11:02:40 +0900

    [Silver III 11441] Title: 합 구하기, Time: 228 ms, Memory: 126268 KB -NKLCBHub

--- acmicpc/#11441 합 구하기/합 구하기.py
+++ acmicpc/#11441 합 구하기/합 구하기.py
@@ -3,7 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 array_n = list(map(int,input().split()))
-loop = int(input())
-for _ in range(loop):
+prefix_sum = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    prefix_sum[i] = prefix_sum[i - 1] + array_n[i - 1]
+
+m = int(input())
+for _ in range(m):
     i, j = map(int,input().split())
-    print(sum(array_n[i-1:j]))
+    print(prefix_sum[j] - prefix_sum[i - 1])
