41d631d8cc2ca2738c846c630a2de30e98fdd9a8
d982h8st7 <sinnarver15@gmail.com>
2023-10-08 05:29:30 +0900

    dd

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        answer = True
-        temp = list(str(x))
-        if temp[0:] == temp[::-1]:
-            answer = True
-        else:
-            answer = False
-        return answer
