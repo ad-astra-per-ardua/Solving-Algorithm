f1acef32ac682524aba98d2be9bb7d4d8730d114
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-10-08 04:53:56 +0900

    [Bronze III 29720] Title: 그래서 님 푼 문제 수가?, Time: 80 ms, Memory: 39964 KB -NKLCBHub

---
+++ acmicpc/#29720 그래서 님 푼 문제 수가?/그래서 님 푼 문제 수가？.py
@@ -0,0 +1,33 @@
+import sys,math,time
+from itertools import permutations,combinations,combinations_with_replacement
+from sys import stdin, stdout
+from math import gcd, floor, sqrt, log, perm, comb
+from collections import defaultdict,deque
+from bisect import bisect_left as bl, bisect_right as br
+from heapq import heapify, heappush, heappop
+from functools import lru_cache, reduce
+from types import GeneratorType
+def ceil(x): return int(x) if(x == int(x)) else int(x)+1
+def ceildiv(x, d): return x//d if(x % d == 0) else x//d+1
+def flush(): return stdout.flush()
+def stdstr(): return stdin.readline()
+
+mod = 1_000_000_007
+# sys.setrecursionlimit(100_000_000)
+input = lambda: stdstr().strip()
+############## main code goes here ##############
+
+def main():
+    n, m, k = map(int, input().split())
+
+    low = n - m * k
+    high = low + m - 1
+
+    low = max(low, 0)
+    high = max(high, 0)
+
+    print(low, high)
+
+
+if __name__ == "__main__":
+    main()
