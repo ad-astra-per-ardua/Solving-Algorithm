dd4f8a81371453a097273db15b1a69d2a2d07513
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-10-08 04:54:01 +0900

    [Bronze III 29729] Title: 가변 배열, Time: 160 ms, Memory: 39552 KB -NKLCBHub

---
+++ acmicpc/#29729 가변 배열/가변 배열.py
@@ -0,0 +1,39 @@
+import sys,math,time
+from itertools import permutations,combinations,combinations_with_replacement
+from sys import stdin, stdout
+from math import gcd, floor, sqrt, log, perm, comb
+from collections import defaultdict,deque
+from bisect import bisect_left as bl, bisect_right as br
+from heapq import heapify, heappush, heappop
+from functools import lru_cache, reduce
+from types import GeneratorType
+def ceil(x): return int(x) if(x == int(x)) else int(x)+1
+def ceildiv(x, d): return x//d if(x % d == 0) else x//d+1
+def flush(): return stdout.flush()
+def stdstr(): return stdin.readline()
+
+mod = 1_000_000_007
+# sys.setrecursionlimit(100_000_000)
+input = lambda: stdstr().strip()
+############## main code goes here ##############
+
+def main():
+    s0, n, m = map(int, input().split())
+
+    s = s0
+    u = 0
+
+    for _ in range(n + m):
+        x = int(input())
+
+        if x == 1:
+            u += 1
+            if u > s:
+                s *= 2
+        else:
+            u -= 1
+
+    print(s)
+
+if __name__ == "__main__":
+    main()
