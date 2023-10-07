9b899ab75022fde43814bb5f2316ca981f5a4cd7
SeongMin KIM <50827253+d982h8st7@users.noreply.github.com>
2023-10-08 03:50:54 +0900

    [Platinum II 28688] Title: 로봇융합관 건설, Time: 1504 ms, Memory: 123672 KB -NKLCBHub

---
+++ acmicpc/#28688 로봇융합관 건설/로봇융합관 건설.py
@@ -0,0 +1,26 @@
+import sys,math,time
+from itertools import permutations,combinations,combinations_with_replacement
+from sys import stdin, stdout
+from math import gcd, floor, sqrt, log, perm, comb
+from collections import defaultdict,deque
+from bisect import bisect_left as bl, bisect_right as br
+from heapq import heapify, heappush, heappop
+from functools import lru_cache, reduce
+from types import GeneratorType
+def ceil(x): return int(x) if(x == int(x)) else int(x)+1
+def ceildiv(x, d): return x//d if(x % d == 0) else x//d+1
+def flush(): return stdout.flush()
+def stdstr(): return stdin.readline()
+
+mod = 1_000_000_007
+# sys.setrecursionlimit(100_000_000)
+input = lambda: stdstr().strip()
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    
+    if N <= 2 or M <= 2 or (N % 2 == 1 and M % 2 == 1):
+        print("First")
+    else:
+        print("Second")
