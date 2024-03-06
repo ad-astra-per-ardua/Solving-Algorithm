37b949f9c8230066635cd09f8567a05110e28fd2
SeongMin KIM <50827253+ad-astra-per-ardua@users.noreply.github.com>
2024-03-06 14:20:33 +0900

    [Silver V 3049] Title: 다각형의 대각선, Time: 48 ms, Memory: 35312 KB -NKLCBHub

---
+++ acmicpc/#3049 다각형의 대각선/다각형의 대각선.py
@@ -0,0 +1,27 @@
+import sys,math
+from sys import stdin, setrecursionlimit
+import heapq
+from types import GeneratorType
+def lcm(x, y):
+    return x * y // math.gcd(x, y)
+def perm(n, r):
+    return math.factorial(n) // math.factorial(n-r)
+def comb(n, r):
+    return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+def ceil(x): return int(x) if(x == int(x)) else int(x)+1
+def ceildiv(x, d): return x//d if(x % d == 0) else x//d+1
+# Set direction for graph traversal DFS,BFS Etc... 
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+# MOD = 1_000_000_007
+# setrecursionlimit(10**6)
+input = lambda: stdin.readline().strip()
+
+######### main code goes here #########
+
+n = int(input())
+if n == 3:
+    print(0)
+else:
+    print(comb(n,4))
